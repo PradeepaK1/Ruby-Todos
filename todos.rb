070b2945a0ccd2e45b160034695a08199284d841
PradeepaK1 <ptljkpd@gmail.com>
2021-03-11 00:15:12 +0530

    my first commit

---
+++ todos.rb
@@ -0,0 +1,12 @@
+#simple todos program
+
+puts "abcd efgh #{1 + 10}"
+puts "What is your next todo?"
+next_todo = gets 
+
+puts "So, this is your next todo:"
+puts next_todo
+
+puts "\n\n\n" #carriage return
+
+puts "[ ] #{next_todo}" #string interpolation
